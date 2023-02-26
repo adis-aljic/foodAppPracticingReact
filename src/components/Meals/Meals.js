252e32646fe2e99f818071cff29dadf4dd9b58c4
Adis Aljic <adis.qm@gmail.com>
2023-02-26 15:20:56 +0100

    Added components for items and meals

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react/cjs/react.development";
+import MealsSummary from "./MealsSummary";
+import AvaiableMeals from "./AvaiableMeals"
+const Meals = () => {
+return <Fragment>
+<MealsSummary />
+<AvaiableMeals></AvaiableMeals>
+</Fragment>
+
+
+};
+
+export default Meals;
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,23 @@
+import classes from "./MealsSummary.module.css"
+
+const MealsSummary = () =>{
+
+return (
+
+<section className={classes.summary}>
+
+<h2>
+    Delicious food delivered to you
+</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore facilis consequatur laborum cupiditate eum
+     exercitationem temporibus eaque amet possimus. Soluta, hic culpa. Quia dolores aspernatur itaque quaerat laudantium iusto aut.</p>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, deleniti quos hic eligendi maiores temporibus odio ratione
+     unde culpa cupiditate totam aperiam vel voluptatem, error iste. Error libero repellat dolorum?</p>
+</section>
+
+)
+
+
+}
+
+export default MealsSummary
